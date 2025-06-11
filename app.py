bc8fe0b0679f8131274dc669996fb0fd76bc558f
Sam <scs45@uw.edu>
2025-06-11 12:45:10 -0700

    base app html complete

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from functions import process_events_for_next_3_days
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    events = []
-    error = None
     if request.method == "POST":
-        city = request.form["city"]
+        city = request.form.get("city_name")
+        if city:
+            return redirect(url_for("results", city_name=city))
+    return render_template("index.html")
+
+@app.route("/results")
+def results():
+    city = request.args.get("city_name", None)
+    if city:
         events, error = process_events_for_next_3_days(city)
-    return render_template("index.html", events=events, error=error)
+        result = f"Results for city: {city}"
+    else:
+        result = "No city specified."
+    return render_template("results.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
